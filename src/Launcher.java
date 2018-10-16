3b2f3acd1b9fce17439bc0016ee70c3a545acaf9
bruhla <bruhla.info@gmail.com>
2018-10-16 15:56:49 +0200

    Dice done

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,12 +6,10 @@ public class Launcher {
         Process p1 = new Process("P1");
         Process p2 = new Process("P2");
         Process p3 = new Process("P3");
-        Process p4 = new Process("P4");
-        Process p5 = new Process("P5");
-        Process p6 = new Process("P6");
+
 
         try{
-            Thread.sleep(5000);
+            Thread.sleep(3000);
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -19,15 +17,12 @@ public class Launcher {
         p1.stop();
         p2.stop();
         p3.stop();
-        p4.stop();
-        p5.stop();
-        p6.stop();
+
+
         p1.waitStopped();
         p2.waitStopped();
         p3.waitStopped();
-        p4.waitStopped();
-        p5.waitStopped();
-        p6.waitStopped();
+
 
 
         System.out.println("Final clocks: " + p1.getClock() + ", " + p2.getClock() + ", " + p3.getClock());
--- src/Process.java
+++ src/Process.java
@@ -3,6 +3,8 @@ import com.google.common.eventbus.Subscribe;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Process  implements Runnable {
@@ -15,6 +17,7 @@ public class Process  implements Runnable {
     private static int nbProcess = 0;
     private int id = Process.nbProcess++;
     private int ack = 0;
+    private ArrayList<Object> broadcastData = new ArrayList<>();
 
     public Process(String name){
 
@@ -45,13 +48,17 @@ public class Process  implements Runnable {
                     }
                 }
                 t.setPayload((this.id + 1) % Process.nbProcess);
-                bus.postEvent(t);
+                if ( !this.dead ) {
+                    bus.postEvent(t);
+                }
                 stateToken = "null";
 
             }else{
 
                 t.setPayload((this.id + 1) % Process.nbProcess);
-                bus.postEvent(t);
+                if ( !this.dead ) {
+                    bus.postEvent(t);
+                }
             }
         }
 
@@ -78,9 +85,9 @@ public class Process  implements Runnable {
     public void broadcast(Object o)
     {
         this.clock++;
-        System.out.println(Thread.currentThread().getName() + " clock : " + this.clock);
+        //System.out.println(this.thread.getName() + " clock : " + this.clock);
         AbstractMessage m1 = new BroadcastMessage(o,this.clock, Thread.currentThread().getName());
-        System.out.println(Thread.currentThread().getName() + " send : " + m1.getPayload());
+        System.out.println(this.thread.getName() + " send in broadcast: " + m1.getPayload()+ ", with clock at " + this.clock);
         bus.postEvent(m1);
     }
 
@@ -89,7 +96,8 @@ public class Process  implements Runnable {
     public void onBroadcast(BroadcastMessage m){
         //receive
         if(!m.getSender().equals(this.thread.getName())){
-            System.out.println(Thread.currentThread().getName() + " receives: " + m.getPayload() + " for " + this.thread.getName());
+            System.out.println(this.thread.getName() + " receives in broadcast: " + m.getPayload());
+            broadcastData.add(m.getPayload());
             if(m.getClock() > this.clock)
             {
                 this.clock = m.getClock()+1;
@@ -105,7 +113,7 @@ public class Process  implements Runnable {
 
     public void sendTo(Object o, int to) {
         this.clock++;
-        System.out.println(this.thread.getName() + " send [" + o + "] to [id " + to + "], with clock at " + this.clock);
+        System.out.println(this.thread.getName() + " send [" + o + "] to [ P" + to+1 + "], with clock at " + this.clock);
         MessageTo m = new MessageTo(o, this.clock, to);
         bus.postEvent(m);
     }
@@ -113,11 +121,11 @@ public class Process  implements Runnable {
     @Subscribe
     public void onReceive(MessageTo m) {
         if (this.id == m.getIdDest()) { // the current process is the destination
-            System.out.println(this.thread.getName() + " receives: " + m.getPayload()  + " for " + this.thread.getName());
+            System.out.println(this.thread.getName() + " receives in one to one: " + m.getPayload());
             this.clock = Math.max(this.clock, m.getClock());
             this.clock++;
 
-            System.out.println("New clock (" + this.thread.getName() + "): " + this.clock);
+            //System.out.println("New clock (" + this.thread.getName() + "): " + this.clock);
 
         }
     }
@@ -127,7 +135,7 @@ public class Process  implements Runnable {
         ack = 0;
 
         this.clock++;
-        System.out.println(Thread.currentThread().getName() + " sends synchronization, with clock at " + this.clock);
+        System.out.println(this.thread.getName() + " sends synchronization, with clock at " + this.clock);
         MessageSynchro m = new MessageSynchro(this.clock, this.id);
         bus.postEvent(m);
 
@@ -146,10 +154,10 @@ public class Process  implements Runnable {
 
     @Subscribe
     public void onSynchronize(MessageSynchro m) {
-        System.out.println(Thread.currentThread().getName() + " receives synchro message from id " + m.getFrom());
+        System.out.println(this.thread.getName() + " receives synchro message from P" + (m.getFrom()+1));
         this.clock = Math.max(this.clock, m.getClock());
         this.clock++;
-        System.out.println("New clock (" + Thread.currentThread().getName() + ", id " + this.id + "): " + this.clock);
+        //System.out.println("New clock (" + this.thread.getName()+"): " + this.clock);
 
         // received ACK
         ack++;
@@ -158,7 +166,7 @@ public class Process  implements Runnable {
 
     public void run(){
 
-        System.out.println(Thread.currentThread().getName() + " id: " + this.id);
+        System.out.println(this.thread.getName() + " id: " + this.id);
 
         if(this.id == Process.nbProcess-1){
             Token t = new Token(this.id);
@@ -174,29 +182,55 @@ public class Process  implements Runnable {
                 – Celui qui a tiré la plus grande valeur demande une section critique pour écrire son numéro dans un fichier
                 – Tous les Process se synchronisent avant de relancer le dé.*/
 
+                broadcastData.clear();
                 Thread.sleep(500);
                 int die = throwDie(6);
+                System.out.println(this.thread.getName() + " : " + die);
+                broadcastData.add(die);
                 broadcast(die);
 
-                // TODO plus grande valeur
+                System.out.println(broadcastData);
 
-                //request();
+                synchronize();
 
-                // écrire dans fichier
-                String result = die + " \n"; // TODO plus grande valeur
-                File file = new File("resultats.txt");
-                if (!file.exists()) {
-                    file.createNewFile();
+                int max = 0;
+                for(int i = 0; i< broadcastData.size()-1 ;i++)
+                {
+                    if((int)broadcastData.get(i) > (int)broadcastData.get(i+1))
+                    {
+                        max = (int)broadcastData.get(i);
+                    }
+                    else
+                    {
+                        max = (int)broadcastData.get(i+1);
+                    }
+                    System.out.println("Max " + max);
                 }
-                FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
-                BufferedWriter bw = new BufferedWriter(fw);
 
-                bw.write(result);
+                System.out.println(broadcastData);
 
-                bw.close();
-                fw.close();
+                if(max == die) {
 
-                //release();
+                    System.out.println(this.thread.getName() + " : J'ai gagné");
+
+                    request();
+
+                    // écrire dans fichier
+                    String result = die + " \n";
+                    File file = new File("resultats.txt");
+                    if (!file.exists()) {
+                        file.createNewFile();
+                    }
+                    FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+
+                    bw.write(result);
+
+                    bw.close();
+                    fw.close();
+
+                    release();
+                }
 
                 synchronize();
 
@@ -209,7 +243,7 @@ public class Process  implements Runnable {
         // liberation du bus
         this.bus.unRegisterSubscriber(this);
         this.bus = null;
-        System.out.println(Thread.currentThread().getName() + " stopped");
+        System.out.println(this.thread.getName() + " stopped");
         this.dead = true;
     }
 
